793684a6d4575707a52a928ef594599ca99f4468
manlikeghost <ekpoabasiama@gmail.com>
2021-03-10 00:28:10 +0100

    finished Ui for vue test app

---
+++ src/components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+    <header>
+       <h1>{{title}}</h1> 
+       <Button 
+        @btn-click="$emit('toggle-add-task')" 
+        :text="displayAddTask ? 'Close Me' : 'Add new'" 
+        :color="displayAddTask ? 'red' : 'purple'" 
+        />
+    </header>
+</template>
+
+<script>
+
+    import Button from './Button'
+
+
+    export default {
+        name: 'Header',
+        props: {
+            title: String,
+            displayAddTask: Boolean,
+        },
+        components: {
+            Button
+        }
+    }
+</script>
+
+<style scoped>
+    header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom:20px;
+    }
+</style>
